9cb5c08727ee9c5aa221f74a5d0a159b10a66860
Vito416 <vito416@protonmail.com>
2025-12-05 16:33:04 +0100

    Regenerate PHP from schema

--- schema/020_indexes.postgres.sql
+++ schema/020_indexes.postgres.sql
@@ -2,4 +2,6 @@
 -- engine: postgres
 -- table:  rbac_repositories
 
+CREATE UNIQUE INDEX IF NOT EXISTS ux_rbac_repositories_name ON rbac_repositories (name);
+
 CREATE INDEX IF NOT EXISTS idx_rbac_repos_status ON rbac_repositories (status);
